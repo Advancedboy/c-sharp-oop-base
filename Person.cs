ccc9730b1cb6495991f9116c8642400c19aeb59a
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-05-28 12:26:41 +0300

    C Sharp oop base

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Constructors
+{
+    internal class Person
+    {
+
+        public string SecondName { get; protected set; }
+        public string Name { get; protected set; }
+        public int Age { get; protected set; }
+
+        public Person(string secondName, string name, int age)
+        {
+            if (age > 0 && age < 150)
+            {
+                Age = age;
+            }
+            SecondName = secondName;
+            Name = name;
+        }
+
+
+
+        //public string FullName
+        //{
+        //    get
+        //    {
+        //        return SecondName + " " + Name;
+        //    }
+        //}
+
+        //public string ShortName
+        //{
+        //    get
+        //    {
+        //        return $"{SecondName} {Name.Substring(0, 1)}.";
+        //    }
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Pavel";
+            var lastName = "Haikvch";
+            var age = 16;
+            var p = new Person(lastName, name, age);
+            //p.Name = "Pavel";
+            //p.SecondName = "Haikvch";
+            Console.WriteLine($"Name: {p.Name}.");
+            Console.WriteLine($"Second Name: {p.SecondName}.");
+            Console.WriteLine($"Age: {p.Age} years old.");
+            //Console.WriteLine("Full Name: " + p.FullName);
+            //Console.WriteLine("Short Name: " + p.ShortName);
+            //Console.ReadKey();
+
+        }
+    }
+}
